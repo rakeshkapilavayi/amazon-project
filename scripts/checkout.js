a4a9179de983545a80d17a7e0d6a8985a9fa4ce4
Rakesh <rakeshkapilavai978@gmail.com>
2024-12-06 12:23:34 +0530

    delete products from html when clicking delete

--- scripts/checkout.js
+++ scripts/checkout.js
@@ -17,7 +17,7 @@ cart.forEach((cartItem) => {
 
     cartSummaryHTML+=
     `
-    <div class="cart-item-container">
+    <div class="cart-item-container js-cart-item-container-${matchingproduct.id}">
             <div class="delivery-date">
               Delivery date: Tuesday, June 21
             </div>
@@ -102,6 +102,8 @@ document.querySelectorAll('.js-delete-link')
     link.addEventListener('click', () => {
       const productid = link.dataset.productId;
       removefromcart(productid);
-      console.log(cart);
+
+      const container = document.querySelector(`.js-cart-item-container-${productid}`);
+      container.remove();
     });
    });
