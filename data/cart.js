9996c87bd22a534d4d38d4bd9f55d9bcbef3be4e
Rakesh <rakeshkapilavai978@gmail.com>
2024-12-07 12:30:29 +0530

    Make delivery option Interactive

--- data/cart.js
+++ data/cart.js
@@ -50,3 +50,17 @@ export function Addtocart(productId){
     cart=newCart;
     savetoStorage();
   }
+
+  export function updateDeliveryOption(productId, deliveryOptionId){
+    let matchingItem;
+  
+    cart.forEach((cartItem) => {
+        if(productId === cartItem.productId){
+            matchingItem = cartItem;
+        }
+    });
+
+    matchingItem.deliveryOptionId = deliveryOptionId;
+
+    savetoStorage();
+  }
