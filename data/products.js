1c338fab3ad2455fd21b9b78b283f4bd9c360eec
Rakesh <rakeshkapilavai978@gmail.com>
2024-12-09 14:08:35 +0530

    convert products into classes

--- data/products.js
+++ data/products.js
@@ -1,3 +1,5 @@
+import {formatCurrency}  from '../scripts/utils/money.js';
+
 export function getProducts(productId){
   let matchingproduct;
 
@@ -10,7 +12,29 @@ export function getProducts(productId){
       return matchingproduct;
 }
 
+class Product{
+  id;
+  image;
+  name;
+  rating;
+  priceCents;
 
+  constructor(productDetails){
+    this.id = productDetails.id;
+    this.image = productDetails.image;
+    this.name = productDetails.name;
+    this.rating = productDetails.rating;
+    this.priceCents = productDetails.priceCents;
+  }
+
+  getStarsUrl(){
+    return `images/ratings/rating-${this.rating.stars * 10}.png`;
+  }
+
+  getPrice(){
+    return `$${formatCurrency(this.priceCents)}`;
+  }
+}
 
 export const products = [
   {
@@ -671,4 +695,6 @@ export const products = [
       "mens"
     ]
   }
-];
+].map((productDetails) => {
+    return new Product(productDetails);
+});
